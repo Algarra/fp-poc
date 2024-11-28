97f7f03eb6c6a51d31e3ee8f87f24a916adb7cdf
Daniel Navarro <daniel.navarro@LUX-MBP-0467.fritz.box>
2024-11-28 11:03:19 +0100

    fix: disable no explicit any

--- app/components/FPGenerator/FPGenerator.tsx
+++ app/components/FPGenerator/FPGenerator.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable  @typescript-eslint/no-explicit-any */
+
 "use client";
 import { useConfigStore } from "@/app/zustand/useConfigStore";
 import { useEffect, useState } from "react";
